cee41611f97fb33c509d204680c2efacc56588d1
Steve <steveocrypto@adams66.33mail.com>
2021-11-01 09:53:24 -0500

    Enable cutscene

--- src/classes/Overworld.ts
+++ src/classes/Overworld.ts
@@ -65,12 +65,12 @@ export class Overworld {
 
     this.startGameLoop();
 
-    // this.map.startCutscene([
-    //   { who: "hero", type: "walk", direction: "down" },
-    //   { who: "hero", type: "walk", direction: "down" },
-    //   { who: "npc1", type: "walk", direction: "left" },
-    //   { who: "npc1", type: "walk", direction: "left" },
-    //   { who: "npc1", type: "stand", direction: "up", time: 800 },
-    // ]);
+    this.map.startCutscene([
+      { who: "hero", type: "walk", direction: "down" },
+      { who: "hero", type: "walk", direction: "down" },
+      { who: "npc1", type: "walk", direction: "left" },
+      { who: "npc1", type: "walk", direction: "left" },
+      { who: "npc1", type: "stand", direction: "up", time: 800 },
+    ]);
   }
 }
