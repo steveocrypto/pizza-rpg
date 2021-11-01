abc3685124cb26615d4c82bc913c6caad93e514b
Steve <steveocrypto@adams66.33mail.com>
2021-11-01 16:22:49 -0500

    Remove react code

--- src/classes/TextMessage.ts
+++ src/classes/TextMessage.ts
@@ -19,14 +19,6 @@ export class TextMessage {
   }
 
   createElement() {
-    //     this.element = createElement("div", {
-    //       className: "text-message absolute left-0 right-0 bottom-0 h-8 p-4 bg-gray-500 border border-t-800 font-white",
-    //       children: `
-    //       <p class="">${this.text}</p>
-    //       <button class="">Next</button>
-    //   `,
-    //     });
-
     const classes =
       "text-message absolute left-0 right-0 bottom-0 h-8 bg-gray-900 border-t border-gray-900 text-white".split(" ");
     this.element = document.createElement("div");
